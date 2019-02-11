61c0ecde82feffda6ede4e5a6f8445302505f43a
tomoki-hirai <thirai@mikilab.doshisha.c.jp>
2019-02-12 00:48:04 +0900

    場所の追加を実装

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const app = new Vue({
     el: '#app',
     data: {
-      // 使用するデータ
+        // 使用するデータ
+        locations: [
+            { id: 0, name: "清水寺", time: 30 },
+            { id: 1, name: "二条城", time: 30 }
+        ]
     },
     methods: {
-      // 使用するメソッド
+        // 使用するメソッド
+        doAdd: function (event, value) {
+            var name = this.$refs.name;
+            if(!name.value.length){
+                return;
+            }
+            this.locations.push({
+                id: this.locations.length,
+                name: name.value,
+                time: 30
+            });
+            name.value = '';
+        }
     }
-  })
+})
