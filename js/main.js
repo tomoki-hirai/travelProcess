3f6669cc1f608c632cf8c46262c1c86df476fded
tomoki-hirai <thirai@mikilab.doshisha.c.jp>
2019-02-13 00:26:34 +0900

    google map埋め込み

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ const app = new Vue({
             this.locations.splice(index, 1);
             calcTransitTime();
         },
-        changeTime: function(item){
+        changeTime: function (item) {
             console.log('change');
             calcTime();
         }
@@ -49,41 +49,41 @@ function calcTime() {
         sumtime += parseInt(app.locations[i].time);
     }
     // 移動時間
-    for(let key in app.transitTime){
-        if(app.transitTime[key] == undefined){
+    for (let key in app.transitTime) {
+        if (app.transitTime[key] == undefined) {
             continue;
         }
         sumtime += parseInt(app.transitTime[key].time);
     }
     console.log(sumtime);
-    
-    let hour = app.startTime + parseInt(sumtime/60);
-    let minute = sumtime%60;
+
+    let hour = app.startTime + parseInt(sumtime / 60);
+    let minute = sumtime % 60;
     console.log(hour + ',' + minute);
 
-    let hourStr = ( '00' + hour ).slice( -2 );
-    let minuteStr = ( '00' + minute ).slice( -2 );
+    let hourStr = ('00' + hour).slice(-2);
+    let minuteStr = ('00' + minute).slice(-2);
 
     app.endTime = hourStr + ':' + minuteStr;
 }
 
-function calcTransitTime(){
+function calcTransitTime() {
     app.transitTime = {};
-    if(app.locations.length>=2){
-        for(let i=0;i<app.locations.length-1;i++){
-            doget(app.locations[i],app.locations[i+1]);
+    if (app.locations.length >= 2) {
+        for (let i = 0; i < app.locations.length - 1; i++) {
+            doget(app.locations[i], app.locations[i + 1]);
         }
     }
 }
 
-function doget(origin,destination) {
+function doget(origin, destination) {
     console.log("push");
     getGoogleMap(origin.name, destination.name).done(function (result) {
         console.log('end');
         let min = result.routes[0].legs[0].duration.value / 60;
         let text = result.routes[0].legs[0].duration.text;
         console.log(result.routes[0].legs[0].duration.value);
-        app.$set(app.transitTime,String(origin.id),{id: origin.id , origin: origin.name, destination: destination.name,time: min,text: text});
+        app.$set(app.transitTime, String(origin.id), { id: origin.id, origin: origin.name, destination: destination.name, time: min, text: text });
         console.log(app.transitTime);
 
         calcTime();
@@ -105,67 +105,35 @@ function getGoogleMap(originName, destinationName) {
         }
     });
 }
-function reqListener () {
+function reqListener() {
     console.log(this.responseText);
-  }
-$(function(){
-    var param = 'key=AIzaSyAgSs7fjrKUT1qr7vGYULTMaip0dRj31y8' + '&origin=' + encodeURIComponent('kyoto') + '&destination=' + encodeURIComponent('tokyo');
-    var url = 'https://maps.googleapis.com/maps/api/directions/json?' + param;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', reqListener);
-    xhr.send(null);
-    // function start() {
-    //     // 2. Initialize the JavaScript client library.
-    //     gapi.client.init({
-    //       'apiKey': 'AIzaSyAgSs7fjrKUT1qr7vGYULTMaip0dRj31y8',
-    //       // clientId and scope are optional if auth is not required.
-    //     //   'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
-    //     //   'scope': 'profile',
-    //     }).then(function() {
-    //       // 3. Initialize and make the API request.
-    //       return gapi.client.request({
-    //         'path': 'https://maps.googleapis.com/maps/api/directions/json',
-    //         'params': {
-    //                         origin: 'kyoto',
-    //                         destination: 'tokyo',
-    //                         key: 'AIzaSyAgSs7fjrKUT1qr7vGYULTMaip0dRj31y8',
-    //                         sortOrder: 'LAST_NAME_ASCENDING'
-    //                     },
-    //       })
-    //     }).then(function(response) {
-    //       console.log(response.result);
-    //     }, function(reason) {
-    //       console.log('Error: ' + reason.result.error.message);
-    //     });
-    //   };
-    //   // 1. Load the JavaScript client library.
-    //   gapi.load('client', start);
-    // gapi.load('client', {
-    //     callback: function() {
-    //       // Handle gapi.client initialization.
-    //       var restRequest = gapi.client.request({
-    //         'path': 'https://maps.googleapis.com/maps/api/directions/json',
-    //         'params': {
-    //             origin: 'kyoto',
-    //             destination: 'tokyo',
-    //             key: 'AIzaSyAgSs7fjrKUT1qr7vGYULTMaip0dRj31y8'
-    //         }
-    //       });
-    //       console.log(restRequest);
-    //       restRequest.execute(logResults)
-    //     },
-    //     onerror: function() {
-    //       // Handle loading error.
-    //       alert('gapi.client failed to load!');
-    //     },
-    //     timeout: 5000, // 5 seconds.
-    //     ontimeout: function() {
-    //       // Handle timeout.
-    //       alert('gapi.client could not load in a timely manner!');
-    //     }
-    //   });
+}
+$(window).load(function () {
+        var directionsService = new google.maps.DirectionsService;
+        // Optionally create a map
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7,
+            center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
+
+        directionsService.route({
+                origin: 'oklahoma city, ok',
+                destination: 'chicago, il',
+                travelMode: 'DRIVING'
+        }, function(response, status) {
+            if (status === 'OK') {
+                // Pass data to the map
+                directionsDisplay.setDirections(response);
+
+                // See the data in the console
+                console.log(response);
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
 });
-function logResults(json,raw){
+function logResults(json, raw) {
     console.log(json);
-  }
+}
